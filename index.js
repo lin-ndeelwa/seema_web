a631c1c652d875d2a6ad82d046fa692cded88208
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-04-16 02:41:01 +0200

    build

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const app = express();
 app.use(express.static('./'))
 let httpServer = require('http').createServer(app)
 
+let port = 3000 || process.env.PORT
+
 const api = new ParseServer({
     databaseURI: 'mongodb://seema:amees@ds121189.mlab.com:21189/seema', // Connection string for your MongoDB database
     cloud: './cloud/main.js', // Absolute path to your Cloud Code
     appId: 'myAppId',
     masterKey: 'myMasterKey', // Keep this key secret!
     fileKey: 'optionalFileKey',
-    serverURL: 'http://localhost:3000/parse', // Don't forget to change to https if needed
+    serverURL: `http://localhost:${port}/parse`, // Don't forget to change to https if needed
     liveQuery:{
         classNames:['thing','anything']
     }
@@ -24,7 +26,7 @@ ParseServer.createLiveQueryServer(httpServer,
 
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
-let port = 3000 || process.env.PORT
+
 console.log(process.env.PORT)
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port 1337.');
